459c264655d5b124db01b354611367f3ec4f0a43
yunifp <darrenarmani666@gmail.com>
2026-03-03 13:36:05 +0700

    s

--- micro-rbac/src/config/index.ts
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
-export const config = {
-    internalApiKey: process.env.INTERNAL_API_KEY || 'secret_internal_key',
-    services: {
-        logs: process.env.LOGS_SERVICE_URL || 'http://localhost:3003'
-    }
-};
